937012877d33029f9b576122c1af8455d8d3f593
Daniel Pereira <daniel.prr.azevedo@gmail.com>
2020-10-10 17:52:05 -0300

    chore: adicionando a rota de login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    loadChildren: () =>
+      import('./pages/login/login.module').then((m) => m.LoginPageModule),
+  },
+];
 
 @NgModule({
   imports: [
