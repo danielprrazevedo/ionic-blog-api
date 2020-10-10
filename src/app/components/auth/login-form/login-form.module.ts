cd49f3adf84edc944d3923bf686795e86c2e59f4
Daniel Pereira <daniel.prr.azevedo@gmail.com>
2020-10-10 17:45:33 -0300

    chore: login form component

---
+++ src/app/components/auth/login-form/login-form.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { LoginFormComponent } from './login-form.component';
+
+@NgModule({
+  imports: [ CommonModule, FormsModule, IonicModule,],
+  declarations: [LoginFormComponent],
+  exports: [LoginFormComponent]
+})
+export class LoginFormComponentModule {}
