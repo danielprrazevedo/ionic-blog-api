1e86b265d9e57ee2c7ba94cf2b25965da0ae7774
Daniel Pereira <daniel.prr.azevedo@gmail.com>
2020-10-11 17:53:27 -0300

    chore: user form component

--- src/app/components/usuario-form/usuario-form.component.ts
+++ src/app/components/usuario-form/usuario-form.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  ValidationErrors,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'blog-usuario-form',
@@ -6,9 +13,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./usuario-form.component.scss'],
 })
 export class UsuarioFormComponent implements OnInit {
+  public form: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    const equalTo = (control: AbstractControl): ValidationErrors | null => {
+      if (!this.form) {
+        return null;
+      }
+      const password = this.form.get('password');
+      return password.value !== control.value ? { equalTo: true } : null;
+    };
 
+    this.form = this.fb.group({
+      name: ['', [Validators.required]],
+      username: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(5)]],
+      confirm: ['', [Validators.required, Validators.minLength(5), equalTo]],
+    });
+  }
 }
