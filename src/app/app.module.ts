f86b5ba9348d48a61ca4ae455ad21912c529ae44
Daniel Pereira <daniel.prr.azevedo@gmail.com>
2020-10-11 17:52:16 -0300

    chore: add error msg module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,15 +9,36 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgxMessageErrorModule, MessagesConfig } from 'ngx-message-error';
+
+const customMessages: MessagesConfig = {
+  email: 'O campo deve ser um email',
+  max: 'O valor máximo permitido é ?',
+  maxLength: 'Caracteres máximos permitidos ?/?',
+  maxlength: 'Caracteres máximos permitidos ?/?',
+  min: 'O valor mímimo permitido é ?',
+  minLength: 'Caracteres mínimos permitidos ?/?',
+  minlength: 'Caracteres mínimos permitidos ?/?',
+  required: 'Campo de preenchimento obrigatório',
+  requiredTrue: 'Campo de preenchimento obrigatório',
+};
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NgxMessageErrorModule.forRoot(customMessages),
+  ],
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
