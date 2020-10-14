beaecb3ce121abad3f2b9ded2c749b47fe4c967d
Daniel Pereira <daniel.prr.azevedo@gmail.com>
2020-10-14 15:12:52 -0300

    feat: login guard

---
+++ src/app/guards/login.guard.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  UrlTree,
+  Router,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LoginGuard implements CanActivate {
+  constructor(private route: Router) {}
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    const token = localStorage.getItem('token');
+    if (token) {
+      return true;
+    }
+    this.route.navigate(['/login']);
+    return false;
+  }
+}
