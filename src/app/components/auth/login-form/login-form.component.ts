224b5518a10eb397972a2438d33261a72e17631b
Daniel Pereira <daniel.prr.azevedo@gmail.com>
2020-10-14 12:50:41 -0300

    chore: criou formulário reativo

--- src/app/components/auth/login-form/login-form.component.ts
+++ src/app/components/auth/login-form/login-form.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'blog-login-form',
@@ -6,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-form.component.scss'],
 })
 export class LoginFormComponent implements OnInit {
+  @Output() formSubmit = new EventEmitter();
 
-  constructor() { }
+  form: FormGroup;
 
-  ngOnInit() {}
+  constructor(private fb: FormBuilder) {}
 
+  ngOnInit() {
+    this.form = this.fb.group({
+      username: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(5)]],
+    });
+  }
 }
--- src/app/components/auth/login-form/login-form.module.ts
+++ src/app/components/auth/login-form/login-form.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
 import { LoginFormComponent } from './login-form.component';
+import { NgxMessageErrorModule } from 'ngx-message-error';
 
 @NgModule({
-  imports: [ CommonModule, FormsModule, IonicModule,],
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    ReactiveFormsModule,
+    NgxMessageErrorModule,
+  ],
   declarations: [LoginFormComponent],
-  exports: [LoginFormComponent]
+  exports: [LoginFormComponent],
 })
 export class LoginFormComponentModule {}
