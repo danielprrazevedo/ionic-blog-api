bcf6cc7147a2ae9f961279bb454f6fcdd0d7215b
Daniel Pereira <daniel.prr.azevedo@gmail.com>
2020-10-14 12:08:56 -0300

    chore: add http module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
@@ -33,6 +34,7 @@ const customMessages: MessagesConfig = {
     AppRoutingModule,
     BrowserAnimationsModule,
     NgxMessageErrorModule.forRoot(customMessages),
+    HttpClientModule,
   ],
   providers: [
     StatusBar,
