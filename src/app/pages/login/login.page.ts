b5d5f9e34d7ae602d8245028265bb14770057376
Daniel Pereira <daniel.prr.azevedo@gmail.com>
2020-10-14 12:51:00 -0300

    chore: regra para submit de login

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,6 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -6,7 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-  constructor() {}
+  constructor(private http: HttpClient, private alertCtrl: AlertController) {}
 
   ngOnInit() {}
+
+  login(event: any) {
+    console.log('dados do login', event);
+    this.http.post('http://localhost:3000/auth/login', event).subscribe(
+      (result: any) => {
+        localStorage.setItem('token', result.access_token);
+      },
+      async (err) => {
+        const alert = await this.alertCtrl.create({
+          header: 'Atenção!',
+          message: 'Falha ao realizar login',
+          buttons: ['ok'],
+        });
+        if (err instanceof HttpErrorResponse && err.error.statusCode === 401) {
+          alert.message = 'Login recusado. Verifique usuário e senha';
+        }
+        alert.present();
+        console.log(err);
+      }
+    );
+  }
 }
