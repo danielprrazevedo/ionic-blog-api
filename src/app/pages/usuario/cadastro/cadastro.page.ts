7fe89cb4f8b9f3eacd5e80d6667adb388073bf38
Daniel Pereira <daniel.prr.azevedo@gmail.com>
2020-10-14 12:09:36 -0300

    chore: add insert user

--- src/app/pages/usuario/cadastro/cadastro.page.ts
+++ src/app/pages/usuario/cadastro/cadastro.page.ts
@@ -1,4 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { IUser } from '@database/user';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'blog-cadastro',
@@ -6,10 +10,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cadastro.page.scss'],
 })
 export class CadastroPage implements OnInit {
+  constructor(
+    private http: HttpClient,
+    private toastCtrl: ToastController,
+    private route: Router
+  ) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  async submitForm(user: Partial<IUser>) {
+    const result = await this.http
+      .post<IUser>('http://localhost:3000/user', user)
+      .toPromise<IUser>();
+
+    const toast = await this.toastCtrl.create({
+      message: 'Usuário criado com sucesso',
+    });
+
+    toast.present();
+
+    this.route.navigate(['/login']);
   }
-
 }
